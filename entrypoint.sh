0782ded585ead3c31de74bb416e8c23a3be4aa50
Groovy Sky <groovymail>
2023-03-10 21:15:50 +0000

    working on marketplace

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,13 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-/gmuv -u $1 -r $2 -o cli
+if [ -z "${INPUT_ACCOUNT}" ]; then
+  echo "Account is empty."
+fi
+
+if [ -z "${INPUT_REPOSITORY}" ]; then
+  echo "Repository is empty."
+fi
+
+
+/gmuv -u "${INPUT_ACCOUNT}" -r "${INPUT_REPOSITORY}" -o cli
